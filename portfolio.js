8b7d8d903f3a271e15835b3d75c7b8faf9b2dc3e
sanikakodag2611 <kodagsanika2611@gmail.com>
2026-02-08 17:18:17 +0530

    Simple portfolio design

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+function changeContent(){
+    document.getElementById("heading1").innerHTML="MERN Developer"
+    document.getElementById("heading2").innerHTML="Learn from NoviTECH"
+    document.getElementById("heading3").innerHTML="Active learner"
+    document.getElementById("sub3").style.display="none";
+    document.getElementById("sub4").style.display="block";
+}
